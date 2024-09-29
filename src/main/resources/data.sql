950fdb181a5d58b6a02171cf24af4035be5d879c
Kirill Malskiy <sabbat@mail.ru>
2024-09-29 17:39:32 +0300

    переехал на h2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,15 @@
-INSERT INTO rating_mpa (mpa_id, rating) VALUES (1, 'G');
-INSERT INTO rating_mpa (mpa_id, rating) VALUES (2, 'PG');
-INSERT INTO rating_mpa (mpa_id, rating) VALUES (3, 'PG-13');
-INSERT INTO rating_mpa (mpa_id, rating) VALUES (4, 'R');
-INSERT INTO rating_mpa (mpa_id, rating) VALUES (5, 'NC-17');
+INSERT INTO rating_mpa (mpa_id, rating)
+            VALUES (1, 'G');
+INSERT INTO rating_mpa (mpa_id, rating)
+            VALUES (2, 'PG');
+INSERT INTO rating_mpa (mpa_id, rating)
+            VALUES (3, 'PG-13');
+INSERT INTO rating_mpa (mpa_id, rating)
+            VALUES (4, 'R');
+INSERT INTO rating_mpa (mpa_id, rating)
+            VALUES (5, 'NC-17');
 
-INSERT INTO genres (genre_id, genre) VALUES (1, 'Комедия');
+INSERT INTO genres (genre_id, genre) values (1, 'Комедия');
 INSERT INTO genres (genre_id, genre) VALUES (2, 'Драма');
 INSERT INTO genres (genre_id, genre) VALUES (3, 'Мультфильм');
 INSERT INTO genres (genre_id, genre) VALUES (4, 'Триллер');
